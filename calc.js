a41e1a653831a36acd42798c999233d2ba5482b1
FrolovSergey96 <112880329+FrolovSergey96@users.noreply.github.com>
2022-11-02 21:16:15 +0500

    foreach

--- calc.js
+++ calc.js
@@ -1,52 +1,65 @@
 let input1 = document.getElementById('input1');
-let input2 = document.getElementById('input2');
-let input_res = document.getElementById('result');
+// let input2 = document.getElementById('input2');
+// let input_res = document.getElementById('result');
 
-let button_plus = document.getElementById('plus');
-let button_minus = document.getElementById('minus');
-let button_multi = document.getElementById('multi');
-let button_divide = document.getElementById('divide');
+// let button_1 = document
 
-const Plus = (e) => {
-    e.preventDefault();
-    let arg1 = input1.value;
-    let arg2 = input2.value;
-    let res_arg = Number(arg1) + Number(arg2);
+// let button_plus = document.getElementById('plus');
+// let button_minus = document.getElementById('minus');
+// let button_multi = document.getElementById('multi');
+// let button_divide = document.getElementById('divide');
 
-    input_res.value = res_arg;
-}
+// const Plus = (e) => {
+//     e.preventDefault();
+//     let arg1 = input1.value;
+//     let arg2 = input2.value;
+//     let res_arg = Number(arg1) + Number(arg2);
 
-button_plus.addEventListener('click', Plus);
+//     input_res.value = res_arg;
+// }
 
-const Minus = (e) => {
-    e.preventDefault();
-    let arg1 = input1.value;
-    let arg2 = input2.value;
-    let res_arg = Number(arg1) - Number(arg2);
+// button_plus.addEventListener('click', Plus);
 
-    input_res.value = res_arg;
-}
+// const Minus = (e) => {
+//     e.preventDefault();
+//     let arg1 = input1.value;
+//     let arg2 = input2.value;
+//     let res_arg = Number(arg1) - Number(arg2);
 
-button_minus.addEventListener('click', Minus);
+//     input_res.value = res_arg;
+// }
 
-const Multi = (e) => {
-    e.preventDefault();
-    let arg1 = input1.value;
-    let arg2 = input2.value;
-    let res_arg = Number(arg1) * Number(arg2);
+// button_minus.addEventListener('click', Minus);
 
-    input_res.value = res_arg;
-}
+// const Multi = (e) => {
+//     e.preventDefault();
+//     let arg1 = input1.value;
+//     let arg2 = input2.value;
+//     let res_arg = Number(arg1) * Number(arg2);
 
-button_multi.addEventListener('click', Multi);
+//     input_res.value = res_arg;
+// }
 
-const Divide = (e) => {
-    e.preventDefault();
-    let arg1 = input1.value;
-    let arg2 = input2.value;
-    let res_arg = Number(arg1) / Number(arg2);
+// button_multi.addEventListener('click', Multi);
 
-    input_res.value = res_arg;
-}
+// const Divide = (e) => {
+//     e.preventDefault();
+//     let arg1 = input1.value;
+//     let arg2 = input2.value;
+//     let res_arg = Number(arg1) / Number(arg2);
 
-button_divide.addEventListener('click', Divide);
+//     input_res.value = res_arg;
+// }
+
+// button_divide.addEventListener('click', Divide);
+
+let buttons = document.querySelectorAll('button');
+
+console.log(buttons);
+
+buttons.forEach((item, i)  => {
+    item.addEventListener('click', function(e){
+        e.preventDefault();
+        input1.value+=item.value;
+    })
+});
